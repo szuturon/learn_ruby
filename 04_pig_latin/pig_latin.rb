b21da578c692a4ef3887ff11fd96034f31fce976
Szuturon <ronnyszutu@gmail.com>
2013-08-27 19:18:23 -0400

    finished pig_latin minus bonus

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,11 +1,26 @@
 def translate(str)
-	vowels = ["a", "e", "i", "o", "u"]
+	strings = str.split
+	new_str = ""
+
+	strings.each { |s| new_str << translate_word(s) }
+
+	new_str.chop!
+end
+
+def translate_word(str)
+	vowels = ["a", "e", "i", "o", "u", "qu"]
 	new_str = "" 
 	consonant_part = ""
 
+	
 	while !vowels.include? str[0,1]
+	if "qu".include? str[0,2]
+		consonant_part << str[0,2]
+		str = str[2,str.length]
+	else
 		consonant_part << str[0,1]
 		str = str[1,str.length]
+		end
 	end
-	new_str = str + consonant_part + "ay"
+	new_str = str + consonant_part + "ay "
 end
